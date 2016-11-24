b7ede58229bf7f0ede6ac007fe4aa5bbf7288f77
chy <1003217929@qq.com>
2016-11-24 10:36:46 +0800

    去掉tolua自动生成文件的提交

--- server/src/gamelogic/script/tolua_common.h
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: common
-** Generated automatically by tolua++-1.0.92 on Sat Nov  5 16:25:28 2016.
-*/
-
-/* Exported function */
-TOLUA_API int tolua_common_open (lua_State* tolua_S);
-
